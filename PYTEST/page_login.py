1ab00899cc7435b517814996a91660e08e6c4f8e
Ernesto <er.sennhauser@gmail.com>
2023-02-10 14:51:22 -0300

    New structure

--- PYTEST/page_login.py
+++ PYTEST/page_login.py
@@ -14,44 +14,26 @@ class FunctionsLogin():
 
     def __init__(self, driver):
         self.driver = driver
-
-    def failed_login(self, username, password, message, t=0.1):
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        f = Functions(driver)
-        f.go_to("https://gam-gam-renovacion-backoffice.development.mag.dev/login")
-        f.introduce_text(":r0:", username)
-        f.introduce_text("auth-login-password", password)
-        f.click_by_xpath("//*[@id='__next']/div/div/div/div/div/form/button")
-        error_1 = f.select_element_by_xpath("//*[@id='__next']/div/div/div/div/div/form/div[1]/p")
+        self.f = Functions(driver)
+        self.f.go_to("https://gam-gam-renovacion-backoffice.development.mag.dev/login")
+    def failed_login(self, username, password, message):
+        self.f.introduce_text(":r0:", username)
+        self.f.introduce_text("auth-login-password", password)
+        self.f.click_by_xpath("//*[@id='__next']/div/div/div/div/div/form/button")
+        error_1 = self.f.select_element_by_xpath("//*[@id='__next']/div/div/div/div/div/form/div[1]/p")
         print(error_1.text)
         if error_1.text == message:
             print("\t\t-----PASSED-----\n\n")
         else:
             print("\t\t-----FAILED-----\n\n")
-        driver.close()
 
     def successful_login(self, username, password, product_text):
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        f = Functions(driver)
-        f.go_to("https://gam-gam-renovacion-backoffice.development.mag.dev/login")
-        f.introduce_text(":r0:", username)
-        f.introduce_text("auth-login-password", password)
-        f.click_by_xpath("//*[@id='__next']/div/div/div/div/div/form/button")
-        product_element = f.select_element_by_xpath("//*[@id='__next']/div/main/h3")
-        if product_element.text == product_text:
-            print("\t\t-----PASSED-----\n\n")
-        else:
-            print("\t\t-----FAILED-----\n\n")
-        driver.close()
-
-    def successful_login_opened(self, username, password, product_text):
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        f = Functions(driver)
-        f.go_to("https://gam-gam-renovacion-backoffice.development.mag.dev/login")
-        f.introduce_text("user-name", username)
-        f.introduce_text("password", password)
-        f.click("login-button")
-        product_element = f.select_element_by_xpath("// *[ @ id = 'header_container'] / div[2] / span")
+        self.f.go_to("https://gam-gam-renovacion-backoffice.development.mag.dev/login")
+        self.f.introduce_text(":r0:", username)
+        self.f.introduce_text("auth-login-password", password)
+        self.f.click_by_xpath("//*[@id='__next']/div/div/div/div/div/form/button")
+        product_element = self.f.select_element_by_xpath("//*[@id='__next']/div/main/h3")
         if product_element.text == product_text:
             print("\t\t-----PASSED-----\n\n")
         else:
--- PYTEST/test_login.py
+++ PYTEST/test_login.py
@@ -1,17 +1,25 @@
 from page_login import FunctionsLogin
 
-def test_many_logins():
+def test_failed_logins():
     driver = ""
-    login_function = FunctionsLogin(driver)
     print("\n\t\t-----Test wrong username and wrong password-----")
+    login_function = FunctionsLogin(driver)
     login_function.failed_login("ernesto", "password123", "Email or Password is invalid")
+    login_function.f.driver.close()
 
     print("\t\t-----Test empty username and empty password-----")
+    login_function = FunctionsLogin(driver)
     login_function.failed_login("", "", "username must be at least 6 characters")
+    login_function.f.driver.close()
 
     print("\t\t-----Test wrong password-----")
+    login_function = FunctionsLogin(driver)
     login_function.failed_login("standard_user", "password123", "Email or Password is invalid")
-
+    login_function.f.driver.close()
+def test_successful_logins():
+    driver = ""
     print("\t\t-----Test standard_user login-----")
+    login_function = FunctionsLogin(driver)
     login_function.successful_login("test_1", "Magnetico22!", "Inicio")
+    login_function.f.driver.close()
 
