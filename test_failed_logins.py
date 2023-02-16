d96487d5991410b89c4507a8cbd919b6b8ed8b36
Ernesto <er.sennhauser@gmail.com>
2023-02-16 16:47:51 -0300

    New structure

--- test_failed_logins.py
+++ test_failed_logins.py
@@ -1,5 +1,7 @@
 import pytest
 from Pages.page_login import LoginPage
+import allure
+from allure_commons.types import AttachmentType
 
 
 @pytest.fixture
@@ -7,16 +9,19 @@ def setup_login():
     global login_function
     login_function = LoginPage()
 
+
 def teardown_function():
     login_function.browser.driver.close()
+
 
 @pytest.mark.failed_run
 def test_wrong_username_and_password(setup_login):
     print("\n\t\t-----Test wrong username and wrong password-----")
     login_function.login("ernesto", "password123")
+    allure.attach(login_function.browser.driver.get_screenshot_as_png(), name="user",
+                  attachment_type=AttachmentType.PNG)
     error = login_function.browser.select_element_by_xpath(login_function.username_error)
     assert error.text == "Email or Password is invalid", "ERROR. Unexpected error message."
-
 
 @pytest.mark.failed_run
 def test_empty_username_and_wrong_password(setup_login):
@@ -48,6 +53,3 @@ def test_wrong_password(setup_login):
     login_function.login("standard_user", "password123", )
     error = login_function.browser.select_element_by_xpath(login_function.username_error)
     assert error.text == "Email or Password is invalid", "ERROR. Unexpected error message."
-
-
-
